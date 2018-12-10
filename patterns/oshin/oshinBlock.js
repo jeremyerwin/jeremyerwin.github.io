3b999b293a580b0db2f5cf226651b254951a2390
Jeremy Erwin <jerwin@ponymail.com>
2018-12-10 03:09:10 -0500

    bugfixes

--- patterns/oshin/oshinBlock.js
+++ patterns/oshin/oshinBlock.js
@@ -19,31 +19,40 @@ var makePattern = function() {
 	pat.addPoint("А", 16.2 * scale, 9.6 * scale); //A
 	pat.rightPoint("Б", "А", bust); //B
 	pat.downPoint("И", "Б", backLength); //C
-	pat.angleAngleIntersectionPoint("Ю", "А", -90.0, "И", 0.0); //D
+	pat.angleAngleIntersectionPoint("З", "А", -90.0, "И", 0.0); //D
 	
 	pat.downPoint("Л", "И", hipDepth); //E
-	pat.angleAngleIntersectionPoint("К", "А", -90.0, "Л", 0.0); //F
+	pat.angleAngleIntersectionPoint("Ю", "А", -90.0, "Л", 0.0); //F
 	pat.upPoint("Ж", "И", sideHeight) //G
 	pat.angleAngleIntersectionPoint("Д", "А", -90.0, "Ж", 0.0); //H
+
 	pat.alongPoint("Г", "Б", "Ж", pat.distance("Б", "Ж") / 3.0); //J
+	
 	pat.angleAngleIntersectionPoint("В", "А", -90.0, "Г", 0.0); //K
 	pat.leftPoint("н", "Ж", backWidth); //L
 	pat.angleAngleIntersectionPoint("о", "н", -90.0, "Б", -180.0); //M
-	pat.rightPoint("і", "Д", chestWidth); //N
-	pat.angleAngleIntersectionPoint("М", "А", 0.0, "і", 90.0); //P
+	pat.rightPoint("т", "Д", chestWidth); //N
+	pat.angleAngleIntersectionPoint("М", "А", 0.0, "т", 90.0); 
+	pat.angleAngleIntersectionPoint("Н", "Ю", 0.0, "т", 270.0);
+	pat.leftPoint("с", "т", bust/8 * 1.5);
+	pat.angleAngleIntersectionPoint("р", "А", 0.0, "с", 90.0); //P
+
+
 	
 	pat.printPoints();
 	
 	pat.line("А", "Б");
 	pat.line("Г", "В");
 	pat.line("Ж", "Д");
-	pat.line("И", "Ю");
-	pat.line("Л", "И");
-	pat.line("А", "К");
+	pat.line("И", "З");
+	pat.line("З", "И");
+	pat.line("А", "Ю");
 	pat.line("Б", "Л");
 	pat.line("н", "о");
-	pat.line("і", "М");
-	pat.line("К", "Л");
+	pat.line("с", "р");
+	pat.line("Ю", "Л");
+	pat.line("М", "Н");
+	
 	displaydistance(document);
 }
 makePattern(true);
