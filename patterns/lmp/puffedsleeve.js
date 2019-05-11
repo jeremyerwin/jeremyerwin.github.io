1cca6e1a66425d80671dbb3f771eeda89bc6e703
jeremyerwin <jerwin@ponymail.com>
2019-05-11 05:11:09 -0400

    Update puffedsleeve.js

--- patterns/lmp/puffedsleeve.js
+++ patterns/lmp/puffedsleeve.js
@@ -1,48 +1,47 @@
-var paper=Raphael("container",5000,5000);
-var scale=15;
-var points= new Map();
-var makePattern=function(){
-paper.clear()
+pat=new Pattern();
+pat.document=document;
+var makePattern = function() {
+pat.paper.clear();
+scale=pat.scale;
     myMeasurements={}
 var armholeLength=document.getElementById('armholeLengthSlider').value*scale;
 var elbowLength=document.getElementById('elbowLengthSlider').value*scale;
 var forearmLength=document.getElementById('forearmLengthSlider').value*scale;
-var sleeveLength=document.getElementById('sleeveLengthSlider').value*scale;
-addPoint("A",10.0 *scale,10.0 *scale)
-rightPoint("B","A",armholeLength/2.0);
-downPoint("C","A",armholeLength/12.0+elbowLength);
-angleAngleIntersectionPoint("D","C",0.0,"B",-90.0)
-extendPoint("L","A","C",forearmLength);
-angleAngleIntersectionPoint("M","L",0.0,"B",-90.0)
-midPoint("E","A","B")
-midPoint("F","C","D")
-alongPoint("G","A","C",distance("A","E"));
-displacementPoint("H","B",1.0 *scale,armholeLength/12.0);
-circleLineIntersectionPoint("S","D","F",-forearmLength,90.0);
-perpendicularPoint("Sa","D","S",-(-33.4 *scale));
-intersectionPoint("Sb","C","L","S","Sa")
-extendPoint("I","S","Sb",1.0 *scale);
-alongPoint("Cprime","G","Sb",distance("I","G")/2.0);
-rightPoint("K","Cprime",armholeLength/12.0);
-teeIntersectionPoint("U","H",-180.0,"A","G")
-teeIntersectionPoint("O","G",0.0,"H","D")
-extendPoint("P","F","E",6.0 *scale);
-circleCircleIntersectionPoint("Q","G",3.0 *scale,"K",distance("K","G"),false);
-circleCircleIntersectionPoint("R","H",4.0 *scale,"D",distance("D","H"),true);
-printPoints();
-line("A","L");
-line("B","M");
-line("L","M");
-line("A","B");
-line("C","D");
-line("P","F");
-displacedQuad("D","S",distance("D","S")/2.0,-(-1.0 *scale));
-displacedQuad("K","I",distance("K","I")/2.0,-(-1.0 *scale));
-line("I","S");
-displacedQuad("Q","P",distance("Q","P")/2.0,-(-3.0 *scale));
-displacedQuad("P","R",distance("P","R")/2.0,-(-3.5 *scale));
-line("D","R");
-displacedQuad("Q","K",distance("Q","K")/2.0,-(-1.0 *scale));
+pat.addPoint("A",10.0 *scale,10.0 *scale)
+pat.rightPoint("B","A",armholeLength/2.0);
+pat.downPoint("C","A",armholeLength/12.0+elbowLength);
+pat.angleAngleIntersectionPoint("D","C",0.0,"B",-90.0)
+pat.extendPoint("L","A","C",forearmLength);
+pat.angleAngleIntersectionPoint("M","L",0.0,"B",-90.0)
+pat.midPoint("E","A","B")
+pat.midPoint("F","C","D")
+pat.alongPoint("G","A","C",pat.distance("A","E"));
+pat.displacementPoint("H","B",1.0 *scale,armholeLength/12.0);
+pat.circleLineIntersectionPoint("S","D","F",forearmLength,-90.0);
+pat.perpendicularPoint("Sa","D","S",-(-33.4 *scale));
+pat.intersectionPoint("Sb","C","L","S","Sa")
+pat.extendPoint("I","S","Sb",1.0 *scale);
+pat.alongPoint("Cprime","G","Sb",pat.distance("I","G")/2.0);
+pat.rightPoint("K","Cprime",armholeLength/12.0);
+pat.teeIntersectionPoint("U","H",-180.0,"A","G")
+pat.teeIntersectionPoint("O","G",0.0,"H","D")
+pat.extendPoint("P","F","E",6.0 *scale);
+pat.circleCircleIntersectionPoint("Q","G",3.0 *scale,"K",pat.distance("K","G"),false);
+pat.circleCircleIntersectionPoint("R","H",4.0 *scale,"D",pat.distance("D","H"),true);
+pat.printPoints();
+pat.line("A","L");
+pat.line("B","M");
+pat.line("L","M");
+pat.line("A","B");
+pat.line("C","D");
+pat.line("P","F");
+pat.displacedQuad("D","S",pat.distance("D","S")/2.0,-(-1.0 *scale));
+pat.displacedQuad("K","I",pat.distance("K","I")/2.0,-(-1.0 *scale));
+pat.line("I","S");
+pat.displacedQuad("Q","P",pat.distance("Q","P")/2.0,-(-3.0 *scale));
+pat.displacedQuad("P","R",pat.distance("P","R")/2.0,-(-3.5 *scale));
+pat.line("D","R");
+pat.displacedQuad("Q","K",pat.distance("Q","K")/2.0,-(-1.0 *scale));
 displaydistance(document);
 }
 makePattern(true);
@@ -50,5 +49,4 @@ wireUpScale(document);
 wireUpControl(document,'armholeLength');
 wireUpControl(document,'elbowLength');
 wireUpControl(document,'forearmLength');
-wireUpControl(document,'sleeveLength');
 wireUpDistance(document);
