71f08674c9b7f1887ad543e7348e18f5fccf4add
Jeremy Erwin <jerwin@ponymail.com>
2020-04-11 12:20:16 -0400

    drawers pattern

---
+++ patterns/oshin/oshiblockdrawerswaistband.js
@@ -0,0 +1,37 @@
+pat=new Pattern();
+
+pat.document=document;
+var makePattern = function() {
+
+    pat.paper.clear();
+    scale=pat.scale;
+
+
+    var waist=document.getElementById('waistSlider').value*scale;
+    pat.addPoint("A",100,200)
+    pat.downPoint("B","A",18.0 *scale);
+    pat.rightPoint("C","A",waist/2.0+2.0 *scale);
+    pat.angleAngleIntersectionPoint("D","B",0.0,"C",-90.0)
+    pat.midPoint("E","A","B")
+    pat.midPoint("F","C","D")
+    pat.leftPoint("p1","C",3.0 *scale);
+    pat.circleLSIntersectionPoint("p2","p1","C","D",4.0 *scale);
+    pat.downPoint("p3","B",distance("p1","C"));
+    pat.extendPoint("p5","p3","p1",waist/2.0);
+    pat.perpendicularPoint("p6","p1","p5",4.0 *scale);
+
+        printPoints();
+
+
+    pat.displacedQuad("p2","B",distance("p2","B")/2.0,2.0 *scale);
+    pat.line("p5","p6");
+    pat.line("p5","p1");
+    pat.line("p2","p6");
+    pat.displacedQuad("p1","E",distance("p1","E")/2.0,2.0 *scale);
+    displaydistance(document);
+}
+makePattern(true);
+
+wireUpScale(document);
+wireUpControl(document,'waist');
+wireUpDistance(document);
---
+++ patterns/oshin/oshinblkockdrawers.js
@@ -0,0 +1,44 @@
+/**
+ *
+ */
+
+ pat.document=document;
+ var makePattern = function() {
+
+     pat.paper.clear();
+     scale=pat.scale;
+
+	var scale=document.getElementById('scaleSlider').value;
+	var garmentLength=document.getElementById('drawersLengthSlider').value*scale;
+	var hips=document.getElementById('hipSlider').value*scale;
+	pat.addPoint("C",100,400)
+	pat.rightPoint("D","C",hips/2.0+5.0 *scale);
+	pat.downPoint("R","C",garmentLength);
+	pat.angleAngleIntersectionPoint("S","R",0.0,"D",-90.0)
+	pat.downPoint("T","D",hips/2.0+hips/8.0);
+	pat.rightPoint("p1","T",hips/8.0);
+	pat.leftPoint("p2","D",hips/16.0);
+	pat.leftPoint("p3","S",hips/8.0);
+	pat.upPoint("p4","C",hips/16.0);
+	pat.rightPoint("p5","p1",hips/8.0);
+	pat.extendPoint("p6","p5","D",hips/8.0*1.5);
+	pat.alongPoint("p7","p2","S",hips/8.0*3.0);
+	pat.alongPoint("p8","D","p5",hips/8.0*1.5);
+        printPoints();
+	pat.line("p4","R");
+	pat.line("R","p3");
+	pat.displacedQuad("p1","p3",distance("p1","p3")/2.0,-3.5 *scale);
+	pat.line("C","p2");
+	pat.line("p4","p6");
+	pat.displacedQuad("p7","p1",distance("p7","p1")/2.0,-2.5 *scale);
+	pat.line("p6","p8");
+	pat.displacedQuad("p8","p1",distance("p8","p1")/2.0,2.0 *scale);
+	pat.line("p2","p7");
+    displaydistance(document);
+}
+wireUpScale(document);
+wireUpControl(document,'hip');
+wireUpControl(document,'drawersLength');
+wireUpDistance(document);
+
+makePattern(true);
