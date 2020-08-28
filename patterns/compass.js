c92ab87b8f9ddbb7b2104bf589b030f349cdafb2
Jeremy Erwin <jerwin@ponymail.com>
2020-08-28 02:28:38 -0400

    Update compass.js
    general cleanup. Made sure cardinmal points were correctly generated. Made sure intermediate points were correctly placed

--- patterns/compass.js
+++ patterns/compass.js
@@ -5,28 +5,15 @@ pat.paper.clear();
 scale=pat.scale;
     myMeasurements={}
 var unit=document.getElementById('unitSlider').value*scale;
-pat.addPoint("A",14.02 *scale,10.47 *scale);
-
-
-// pat.directionPoint("NORTH","A",90.0,unit);
-// pat.directionPoint("EAST","A",0.0,unit);
-// pat.directionPoint("SOUTH","A",-90.0,unit);
-// pat.directionPoint("WEST","A",-180.0,unit);
-// pat.directionPoint("SE","A",-45.0,unit);
-// pat.directionPoint("SW","A",-135.0,unit);
-// pat.directionPoint("NW","A",135.0,unit);
-// pat.directionPoint("NE","A",45.0,unit);
-// 
-
-pat.directionPoint("NORTH","A",90.0,unit);
-pat.directionPoint("EAST","A",0.0,unit);
-pat.directionPoint("SOUTH","A",-90.0,unit);
-pat.directionPoint("WEST","A",-180.0,unit);
+pat.addPoint("A",14.02 *scale,10.47 *scale)
+pat.upPoint("NORTH","A",unit);
+pat.rightPoint("EAST","A",unit);
+pat.downPoint("SOUTH","A",unit);
+pat.leftPoint("WEST","A",unit);
 pat.directionPoint("SE","A",-45.0,unit);
 pat.directionPoint("SW","A",-135.0,unit);
 pat.directionPoint("NW","A",135.0,unit);
 pat.directionPoint("NE","A",45.0,unit);
-
 pat.printPoints();
 pat.line("A","NORTH");
 pat.line("A","NE");
@@ -42,3 +29,4 @@ makePattern(true);
 wireUpScale(document);
 wireUpControl(document,'unit');
 wireUpDistance(document);
+
